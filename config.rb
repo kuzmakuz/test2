06e245776d99ea0559136e0543b9ed67ea117d39
kuzmakuz <kuzmakuzmichev@gmail.com>
2018-03-28 00:43:17 +0300

    adjust scss settings (cofig.rb) and base scss structure

---
+++ config.rb
@@ -0,0 +1,12 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'scss'
+images_dir = 'img'
+javascripts_dir = 'js'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
+output_style = :nested
+sourcemap = true
+
